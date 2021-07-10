a1f25be471905bbe9d26756d4100ebc8e38d0652
yash621 <yg17381@gmail.com>
2021-07-10 10:42:38 +0530

    nav bar completed

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,23 @@
+import { useEffect, useState } from "react";
 import "../styles/nav.css";
 
 function NavBar() {
+  const [show, handleShow] = useState(false);
+  const transitionNavBar = () => {
+    if (window.scrollY > 100) {
+      handleShow(true);
+    } else {
+      handleShow(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", transitionNavBar);
+    return () => window.removeEventListener("scroll", transitionNavBar);
+  }, []);
+
   return (
-    <div className="nav_contents nav_black">
+    <div className={`nav_contents ${show && "nav_black"}`}>
       <img
         className="nav_logo"
         src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
