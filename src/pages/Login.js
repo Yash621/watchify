4a4ce4cf2c239c8c945590b0b3194aa704dbc6e7
yash621 <yg17381@gmail.com>
2021-07-20 16:14:26 +0530

    login screen completed

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -13,6 +13,25 @@ function Login() {
       <div className="login_gradient" />
       <div className="login_body">
         <h1>Unlimited movies, TV shows and more.</h1>
+        <h2>Watch anywhere. Cancel anytime.</h2>
+        <div className="login_body_child">
+          <h3>
+            Ready to watch? Enter your email to create or restart your
+            membership.
+          </h3>
+          <div className="input_box">
+            <input
+              type=""
+              placeholder="Email address"
+              className="login_input"
+            />
+            <button className="get_started">
+              <div className="get">
+                <h2>Get Started </h2>
+              </div>
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
